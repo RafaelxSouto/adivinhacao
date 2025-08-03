abd5946c11e76cd4b5089f57f8fdddb09c687d10
rafaelxsouto@proton.me <rafaelxsouto@proton.me>
2025-08-03 10:30:53 -0300

    feat: add Tip component with styling to display tips in the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styles from './app.module.css'
+import { Tip } from './components/Tip'
 import { Header } from './components/Header'
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
     <div className={styles.container}>
       <main>
         <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Tip tip="Uma das Linguagens de programação mais utilizadas" />
       </main>
     </div>
   )
