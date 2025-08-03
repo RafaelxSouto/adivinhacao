ddecb2f0c506effa965b053534521e41664153ba
rafaelxsouto@proton.me <rafaelxsouto@proton.me>
2025-08-03 11:51:50 -0300

    feat: initialize game state and integrate startGame function in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,8 @@
 import styles from './app.module.css'
+import { useEffect, useState } from 'react'
+
+import { WORDS, Challenge } from './utils/words'
+
 import { Button } from './components/Button'
 import { Input } from './components/Input'
 import { Tip } from './components/Tip'
@@ -7,14 +11,30 @@ import { Header } from './components/Header'
 import { LettersUsed } from './components/LettersUsed'
 
 export default function App() {
+  const [letter, setLetter] = useState("")
+  const [attempts, setAttempts] = useState(0)
+  const [challenge, setChallenge] = useState<Challenge | null>(null)
+
   function handleRestartGame() {
     alert('Gatilho executado!')
   }
 
+  function startGame() {
+    const index = Math.floor(Math.random())
+    const randomWord = WORDS[index]
+    setChallenge(randomWord)
+
+    setAttempts(0)
+    setLetter("")
+  }
+  useEffect(() => {
+    startGame()
+  })
+
   return (
     <div className={styles.container}>
       <main>
-        <Header current={5} max={10} onRestart={handleRestartGame} />
+        <Header current={attempts} max={10} onRestart={handleRestartGame} />
         <Tip tip="Uma das Linguagens de programação mais utilizadas" />
         <div className={styles.word}>
           <Letter value="R" />
