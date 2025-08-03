59cb92e10cce3fb518e5597554c528a949280eee
rafaelxsouto@proton.me <rafaelxsouto@proton.me>
2025-08-03 10:52:42 -0300

    feat: add Input component with styling and integrate into App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import styles from './app.module.css'
+import { Input } from './components/Input'
 import { Tip } from './components/Tip'
 import { Letter } from './components/Letter'
 import { Header } from './components/Header'
@@ -19,6 +20,12 @@ export default function App() {
           <Letter value="A" />
           <Letter value="C" />
           <Letter value="T" />
+        </div>
+
+        <h4>Palpite</h4>
+
+        <div>
+          <Input autoFocus maxLength={1} placeholder='?'/>
         </div>
       </main>
     </div>
